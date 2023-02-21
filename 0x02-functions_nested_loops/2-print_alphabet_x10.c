fd8162838a3134b4be11f0199007a547b82334f5
NadaAbdelnasser <102680067+NadaAbdelnasser@users.noreply.github.com>
2023-02-21 15:53:23 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
-mn
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+	int lett, n;
+  while (n < 10)
+  {
+      for (lett = 'a'; lett <= 'z'; lett++)
+      {
+        _putchar(lett);
+      }
+      n++;
+	    _putchar('\n');
+  }
+}
