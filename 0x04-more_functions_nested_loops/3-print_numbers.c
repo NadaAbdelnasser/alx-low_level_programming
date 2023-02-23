cc051be83a9a32127b72a3d21ff1ded3243918c9
NadaAbdelnasser <102680067+NadaAbdelnasser@users.noreply.github.com>
2023-02-23 15:02:17 +0200

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,11 +10,11 @@
 
 void print_numbers(void)
 {
-	int numbers;
+	int number;
 
-	for(numbers = '0'; numbers <= '9' ; numbers++)
+	for(number = '0'; number <= '9' ; number++)
 	{
-		_putchar(numbers);
+		_putchar(number);
 	}
 	_putchar('\n');
 }
