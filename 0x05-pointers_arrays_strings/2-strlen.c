88323d3da82b530945567a606eda9ea2e1ab14cb
NadaAbdelnasser <nadaabdelnasser487@gmail.com>
2023-02-27 02:15:33 -0800

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - a function that Calculate the length of string 
+ * @s: char input
+ * Return: length of string inut
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while(*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return(c);
+	
+}
