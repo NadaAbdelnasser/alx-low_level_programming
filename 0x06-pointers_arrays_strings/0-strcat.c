09e05632758c300e287e6f31d197354fe90966af
NadaAbdelnasser <102680067+NadaAbdelnasser@users.noreply.github.com>
2023-03-03 16:46:51 -0800

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-  int i, j;
+	int i, j;
 
-  i = 0;
-  for (j = 0; dest[j] != '\0'; j++)
-    ;
+	i = 0;
+	for (j = 0; dest[j] != '\0'; j++)
+		;
 
-  while (src[i] != '\0')
-    {
-      dest[j + i] = src[i];
-      i++;
-    }
+	while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
 
-  return (dest);
+	return (dest);
 }
